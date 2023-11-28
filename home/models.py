bf1676bbc12e542912e0f74ea88d4d48f2f746e4
Georgie Hemsley <86372071+GeorgieHemsley@users.noreply.github.com>
2023-11-28 18:56:09 +0000

    home and media

---
+++ home/models.py
@@ -0,0 +1,102 @@
+from django.db import models
+from wagtail.admin.panels import FieldPanel, PageChooserPanel
+from wagtail.models import Page
+from wagtail.fields import StreamField
+from wagtail.snippets.blocks import SnippetChooserBlock
+from django.core.cache import cache
+from django.core.cache.utils import make_template_fragment_key
+
+from streams import blocks
+
+new_table_options = {
+    'minSpareRows': 0,
+    'startRows': 3,
+    'startCols': 3,
+    'colHeaders': True,
+    'rowHeaders': False,
+    'contextMenu': [
+        'row_above',
+        'row_below',
+        '---------',
+        'col_left',
+        'col_right',
+        '---------',
+        'remove_row',
+        'remove_col',
+        '---------',
+        'undo',
+        'redo'
+    ],
+    'editor': 'text',
+    'stretchH': 'all',
+    'autoColumnSize': False,
+}
+
+
+class HomePage(Page):
+    parent_page_types = ["wagtailcore.Page"]
+    subpage_types = ["flex.FlexPage", "services.ServiceListingPage", "contact.ContactPage"]
+    max_count = 1
+    lead_text = models.CharField(
+        max_length=140,
+        blank=True,
+        help_text='Subheading text under the banner title'
+    )
+
+    button = models.ForeignKey(
+        'wagtailcore.Page',
+        blank=True,
+        null=True,
+        related_name='+',
+        help_text='Select an optional page to link to',
+        on_delete=models.SET_NULL
+    )
+
+    button_text = models.CharField(
+        max_length=50,
+        default='Read More',
+        blank=False,
+        help_text='Button text'
+    )
+
+    banner_background_image = models.ForeignKey(
+        'wagtailimages.Image',
+        blank=False,
+        null=True,
+        related_name='+',
+        help_text='The banner background image',
+        on_delete=models.SET_NULL,
+    )
+
+    body = StreamField([
+        ("title", blocks.TitleBlock()),
+        ("cards", blocks.CardsBlock()),
+        ("image_and_text", blocks.ImageAndTextBlock()),
+        ("cta", blocks.CallToActionBlock()),
+        ("testimonial", SnippetChooserBlock(
+            target_model="testimonials.Testimonial",
+            template = "streams/testimonials.html",
+        )),
+        ("pricing_table", blocks.PricingTableBlock(table_options=new_table_options)),
+    ], null=True, blank=True, use_json_field=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel("lead_text"),
+        PageChooserPanel("button"),
+        FieldPanel("button_text"),
+        FieldPanel("banner_background_image"),
+        FieldPanel("body"),
+    ]
+
+    def save(self, *args, **kwargs):
+        key = make_template_fragment_key(
+            "home_page_streams",
+            [self.id],
+        )
+        cache.delete(key)
+
+        return super().save(*args, **kwargs)
+
+
+
+
